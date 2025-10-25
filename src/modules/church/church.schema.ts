dfef1630d50b6477d71b8a65a8f5d572bae76cdb
adetayo adewobi <adewobiadetayo1@gmail.com>
2025-10-25 13:59:28 +0100

    fix: add church type

--- src/modules/church/church.schema.ts
+++ src/modules/church/church.schema.ts
@@ -3,11 +3,15 @@ import { z } from "zod";
 export const churchSchema = z
   .object({
     pastorName: z
-      .string({ required_error: "Username is required" })
+      .string({ required_error: "Pastor's name is required" })
       .min(2, "Username must be at least 2 characters long"),
 
     churchName: z
-      .string({ required_error: "Fullname name is required" })
+      .string({ required_error: "Church name is required" })
+      .min(2, "Last name must be at least 2 characters long"),
+
+    churchType: z
+      .string({ required_error: "Church Type is required" })
       .min(2, "Last name must be at least 2 characters long"),
 
     email: z
