ff4138aff467269f0bbec6712796509eeb2088cd
adetayo adewobi <adewobiadetayo1@gmail.com>
2025-11-29 17:17:11 +0100

    fix: message scheduler

--- src/modules/message/message.provider.ts
+++ src/modules/message/message.provider.ts
@@ -1,6 +1,14 @@
+import MessageService from "../messgaing/message.service";
+
 export class MessageProvider {
   static async sendSms(contact: any, message: string) {
+    const payload = {
+      to: contact.phoneNumber,
+      sms: message,
+    };
     console.log(`📩 [SMS] -> ${contact.phoneNumber}: ${message}`);
+    await MessageService.sendBulkSMSV2(payload);
+
     return true;
   }
 
@@ -11,6 +19,13 @@ export class MessageProvider {
 
   static async sendEmail(contact: any, message: string) {
     console.log(`📧 [Email] -> ${contact.email}: ${message}`);
+    const payload = {
+      to: [contact.email],
+      subject: "Notification",
+      html: message,
+    };
+    await MessageService.sendBulkEmail(payload as any);
+
     return true;
   }
 }
--- src/modules/messgaing/message.service.ts
+++ src/modules/messgaing/message.service.ts
@@ -29,10 +29,11 @@ export interface IResendBulkPayload {
 
 class MessageService {
   static async sendBulkSMSV2(payload: ITermiiBulkPayload) {
+    console.log("Sending bulk SMS via Termii with payload:", payload);
     const maindata = {
       api_key: env.TERMII_API_KEY,
       to: payload.to,
-      // to: ["2349167703400", "2349125778176"], //"2348065108162"],
+      // to: ["2348065108162", "2349167703400"],
       from: "CHURCHSMS",
       sms: payload.sms, //"Hi there, testing Termii bulk send with the new service structure.",
       type: "plain",
@@ -110,7 +111,7 @@ class MessageService {
   static async sendBulkWhatsApp(payload: ITermiiBulkPayload) {
     const maindata = {
       api_key: env.TERMII_API_KEY,
-      to: payload.to,
+      // to: payload.to,
       // to: ["2349167703400", "2349125778176"], //"2348065108162"],
 
       from: "CHURCHSMS", // The Sender ID for your WhatsApp account
@@ -184,6 +185,7 @@ class MessageService {
     const maindata = {
       api_key: env.TERMII_API_KEY,
       to: payload.to,
+
       from: "CHURCHSMS", // The Sender ID for your WhatsApp account
       sms: payload.sms,
       type: "plain",
@@ -329,14 +331,6 @@ class MessageService {
   // }
 
   static async sendBulkEmail(payload: IResendBulkPayload) {
-    console.log({
-      yuii: payload,
-    });
-
-    console.log({
-      xxx_emailTo: payload,
-    });
-
     const emailSubject = payload.subject; //"Bulk Test Email from Church";
     const emailHtml = payload.html;
     const recipients = payload.to;
